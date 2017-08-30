3cab03e34cc05ef3f2513264c96f3d75d1a40217
Yuriy Kolodiy <yuriy.kolodiy@gmail.com>
2017-08-30 18:02:47 -0500

    Add files via upload

--- code/playlist.component.ts
+++ code/playlist.component.ts
@@ -1,11 +1,19 @@
 import {Component} from 'angular2/core';
+import {Video} from './video';
 
 
-@Component ({
-selector: 'playlist',
+@Component ({selector: 'playlist',
 templateUrl: 'app/ts/playlist.component.html',
 inputs: ['videos']
 
 })
 
-export class PlaylistComponent {}
+export class PlaylistComponent {
+	
+onSelect(v:Video) {
+	console.log(JSON.stringify(v))
+
+
+}
+
+}
